25db6a10a68481a1ad658abf6ce41b42ecc96d39
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-14 15:57:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,3 @@
-colors = ["Red","Yellow", "Green"]
-board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts "The first color is #{colors[0]}" 
-  
+  board = [" "," "," "," "," "," "," "," "," "]
 end
